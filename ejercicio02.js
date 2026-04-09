bfbd8b8e26d5d2dcb8ff96cacb6e413439d6a643
rodrigo2412142-glitch <rodrigo2412142@hybridge.education>
2026-04-08 18:52:37 -0600

    Add obtenerPI function to return constant PI value

--- ejercicio02.js
+++ ejercicio02.js
@@ -2,7 +2,10 @@
 // Declara una función llamada "obtenerPI" que retorne el valor 3.1416
 
 //define tu función aquí
-
+function obtenerPI () {
+    const PI = 3.1416;
+    return PI;
+}
 
 // No modificar
 // Exporta la función para que pueda ser utilizada en otros archivos
