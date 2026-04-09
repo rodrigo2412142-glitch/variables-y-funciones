370b6c8da45669853ed84e68b9b11038c0d35e6f
rodrigo2412142-glitch <rodrigo2412142@hybridge.education>
2026-04-08 18:00:43 -0600

    Add variable 'nombre' to return user's name

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,7 +2,8 @@
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    let nombre = "Rodrigo de Lira";
+    return nombre;
 }
 
 // No modificar.
